7572f5cf4fbb3cf0a09613cf17c21edcbc0516b1
mangaldeep95 <mangaldeep95.ms@gmail.com>
2023-10-18 16:43:42 +0530

    All CRUD functionalities done

--- todo_main/ToDo/views.py
+++ todo_main/ToDo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from .models import Task
 # Create your views here.
@@ -6,4 +6,33 @@ def addTask(request):
     if request.method == 'POST':
         task = request.POST['task']
         Task.objects.create(task=task)
+    return redirect('home')
+
+def mark_as_done(request,pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def delete_task(request,pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.delete()
+    return redirect('home')
+def edit_task(request,pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task.task = request.POST['task']
+        task.save()
+        return redirect('home')
+    else:
+        context = {'task':task,
+
+
+                   'pk':pk,}
+        return render(request,'edit_task.html',context)
+
+def mark_as_undone(request,pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.is_completed = False
+    task.save()
     return redirect('home')
