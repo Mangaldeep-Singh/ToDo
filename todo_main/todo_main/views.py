ee262ff344827b306171af2d1cfd9bf46a34f36a
mangaldeep95 <mangaldeep95.ms@gmail.com>
2023-10-18 14:12:32 +0530

    fetch and insert functionality

--- todo_main/todo_main/views.py
+++ todo_main/todo_main/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from ToDo.models import Task
 def home(request):
-    return render(request,'home.html')
+    task = Task.objects.filter(is_completed=False).order_by('-modified_at')
+    completed = Task.objects.filter(is_completed=True).order_by('-modified_at')
+    context = {
+        'tasks': task,
+        'completed': completed,
+    }
+    return render(request,'home.html',context)
